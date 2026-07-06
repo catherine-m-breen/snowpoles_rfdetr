9683ddc1321ca1750352174e6e68e56e7ba5acb1
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2026-07-06 17:02:38 -0400

    fix model path

--- rfdetr_medium_eval.py
+++ rfdetr_medium_eval.py
@@ -10,28 +10,27 @@ from rfdetr import RFDETRSegMedium, RFDETRSegNano
 # --- 1. Paths and Configuration ---
 dataset = '/discover/nobackup/cmbreen/rfdetr_snow/dataset_rfd_detr'
 model_path = '/discover/nobackup/cmbreen/rfdetr_snow/rf-detr-seg-nano.pt'
-outputs = '/discover/nobackup/cmbreen/rfdetr_snow/outputs/nano_outputs'
+viz_out_dir = '/discover/nobackup/cmbreen/rfdetr_snow/outputs/nano_outputs'
 # exp_name = 'SegMedium_bs16_gas2_e200' # Updated to bs16 based on 448 res
 
-os.makedirs(outputs, exist_ok=True)
+os.makedirs(viz_out_dir, exist_ok=True)
 
 # --- 2. Initialize Model ---
 # Ensure resolution matches your dataset!
-model = RFDETRSegNano(resolution=448)
+#model = RFDETRSegNano(resolution=448)
 
 # --- 4. Post-Training Visualization on 5 Examples ---
 # Once training finishes, we load the BEST weights it just saved
 best_model_path = '/discover/nobackup/cmbreen/rfdetr_snow/output/checkpoint_best_total.pth'
 if os.path.exists(best_model_path):
     print(f"Loading best weights for inference from: {best_model_path}")
-    model.load(best_model_path)
+    model = RFDETRSegNano(weights=best_model_path, resolution=448)
     
     # Grab 5 random images from the test set
     test_images_dir = os.path.join(dataset, 'test')
     test_images = glob.glob(os.path.join(test_images_dir, '*.[jJ][pP]*[gG]'))
     sample_images = test_images[:5] # Grab the first 5
     
-    viz_out_dir = os.path.join(outputs, exp_name, 'custom_viz_samples')
     os.makedirs(viz_out_dir, exist_ok=True)
     
     polygon_annotator = sv.PolygonAnnotator()
